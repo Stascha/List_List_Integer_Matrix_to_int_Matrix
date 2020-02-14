24977ad70c836db2ed00d0205934f0e11dad4c53
Stascha <stasarasic@gmail.com>
2020-02-14 10:34:25 +0100

    Update List_List_Integer_Matrix_to_int_Matrix.java

--- List_List_Integer_Matrix_to_int_Matrix.java
+++ List_List_Integer_Matrix_to_int_Matrix.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 public class Convert_Matrix
 {
-    // Funkcija koja konvertuje matricu datu kao List<List<Integer>> 
-    // u matricu u obliku int[][]
+    // Funkcija koja konvertuje matricu datu kao List<List<Integer>> u matricu u obliku int[][]
     public static int[][] List_List_Integer_Matrix_to_int_Matrix(List<List<Integer>> list)
     {
         int[][] array = new int[list.size()][];
@@ -15,7 +14,7 @@ public class Convert_Matrix
 	{
 		for (int j = 0; j < list.get(i).size(); j++) 
 		{
-				array[i][j] = list.get(i).get(j);
+			array[i][j] = list.get(i).get(j);
 		}
 	}
 		
@@ -23,27 +22,36 @@ public class Convert_Matrix
     }
 
 
-    public static void main (String[] args)
+
+    // Funkcija koja pravi matricu kao List<List<Integer>> i popunjava je slučajnim brojevima od 0 do 9
+    public static List<List<Integer>>  Make_A_Matrix_As_a_List_List_Integer(int rows, int columns, int max)
     {
-    	// Pravi se matrica List<List<Integer>> list i
-    	// popunjava se random brojevima od 0 do 9
 	List<List<Integer>> list = new ArrayList<List<Integer>>();
 		
-	for(int i = 0; i< 5; i++)
+	for(int i = 0; i< rows; i++)
 	{
 		List<Integer> integers = new ArrayList<Integer>();
 			
-		for(int j=0; j<5; j++)
+		for(int j=0; j<columns; j++)
 		{
 			Random random = new Random();
-			integers.add(random.nextInt(10));
+			integers.add(random.nextInt(max));
 		}
 			
 		list.add(integers);
+			
 	}
 		
-	// Poziva se funkcija koja konvertuje matricu datu kao List<List<Integer>> 
-	// u matricu u obliku int[][]
+	return list;
+   }	
+    
+
+    public static void main (String[] args)
+    {
+   	// Pravi se matrica List<List<Integer>> list i popunjava se random brojevima od 0 do 9
+   	List<List<Integer>> list =  Make_A_Matrix_As_a_List_List_Integer(5, 5, 9);
+		
+	// Poziva se funkcija koja konvertuje matricu datu kao List<List<Integer>> u matricu u obliku int[][]
 	int[][] a = List_List_Integer_Matrix_to_int_Matrix(list);
 	
 	// Ispiše se matrica	
@@ -51,7 +59,7 @@ public class Convert_Matrix
 	{
 		for (int j = 0; j < a[i].length; j++) 
 		{
-		    	System.out.print(	a[i][j] + " "); 
+		    	System.out.print(  a[i][j] + " "  ); 
 		}
 			
 		System.out.println(	);
