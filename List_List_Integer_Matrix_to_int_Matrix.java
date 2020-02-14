bfb1a7a4c74b437ca9b60e22303e9adbcec9e006
Stascha <stasarasic@gmail.com>
2020-02-14 10:04:11 +0100

    Update List_List_Integer_Matrix_to_int_Matrix.java

--- List_List_Integer_Matrix_to_int_Matrix.java
+++ List_List_Integer_Matrix_to_int_Matrix.java
@@ -1,59 +1,62 @@
 import java.util.*;
 
-public class HelloWorld
+public class Convert_Matrix
 {
-    
+    // Funkcija koja konvertuje matricu datu kao List<List<Integer>> 
+    // u matricu u obliku int[][]
     public static int[][] List_List_Integer_Matrix_to_int_Matrix(List<List<Integer>> list)
     {
         int[][] array = new int[list.size()][];
 		
-		for (int i = 0; i < array.length; i++)
-			array[i] = new int[list.get(i).size()];
+	for (int i = 0; i < array.length; i++)
+		array[i] = new int[list.get(i).size()];
 	
-	    for(int i=0; i<list.size(); i++)
-	    {
-			for (int j = 0; j < list.get(i).size(); j++)
-			{
+	for(int i=0; i<list.size(); i++)
+	{
+		for (int j = 0; j < list.get(i).size(); j++) 
+		{
 				array[i][j] = list.get(i).get(j);
-			}
 		}
+	}
 		
-		return array;
+	return array;
     }
 
 
-public static void main (String[] args)
-{
-		List<List<Integer>> list = new ArrayList<List<Integer>>();
+    public static void main (String[] args)
+    {
+    	// Pravi se matrica List<List<Integer>> list i
+    	// popunjava se random brojevima od 0 do 9
+	List<List<Integer>> list = new ArrayList<List<Integer>>();
 		
-		for(int i = 0; i< 5; i++)
+	for(int i = 0; i< 5; i++)
+	{
+		List<Integer> integers = new ArrayList<Integer>();
+			
+		for(int j=0; j<5; j++)
 		{
-			List<Integer> integers = new ArrayList<Integer>();
-			for(int j=0; j<5; j++)
-			{
-				Random random = new Random();
-				integers.add(random.nextInt(10));
-			}
-			
-			list.add(integers);
+			Random random = new Random();
+			integers.add(random.nextInt(10));
 		}
+			
+		list.add(integers);
+	}
 		
+	// Poziva se funkcija koja konvertuje matricu datu kao List<List<Integer>> 
+	// u matricu u obliku int[][]
+	int[][] a = List_List_Integer_Matrix_to_int_Matrix(list);
 	
-	    int[][] a = List_List_Integer_Matrix_to_int_Matrix(list);
-	
-	
-		
-		for (int i = 0; i < a.length; i++)
+	// Ispiše se matrica	
+	for (int i = 0; i < a.length; i++)
+	{
+		for (int j = 0; j < a[i].length; j++) 
 		{
-			
-		    for (int j = 0; j < a.length; j++) 
-			{
-			    	System.out.print(	a[i][j] + " "); 
-			}
-			
-			System.out.println(	);
+		    	System.out.print(	a[i][j] + " "); 
 		}
+			
+		System.out.println(	);
+	}
 		
-}		
+    }		
 
 }
